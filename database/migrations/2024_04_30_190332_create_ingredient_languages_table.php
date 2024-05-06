e1052e96a3b349a3857370a1794716725b60fbe3
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-06 15:56:57 +0200

    Added deletion cascading to language tables
    I can't alter or remove and readd foreign keys in SQLite, so I would otherwise have to recreate (and redefine) the entire tables in new migrations. I hope this is acceptable.

--- database/migrations/2024_04_30_190332_create_ingredient_languages_table.php
+++ database/migrations/2024_04_30_190332_create_ingredient_languages_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('ingredient_languages', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('ingredient_id')->constrained();
+            $table->foreignId('ingredient_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('description');
             $table->timestamps();
--- database/migrations/2024_04_30_190611_create_burger_languages_table.php
+++ database/migrations/2024_04_30_190611_create_burger_languages_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('burger_languages', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('burger_id')->constrained();
+            $table->foreignId('burger_id')->constrained()->onDelete('cascade');
             $table->string('name');
             $table->string('description');
             $table->timestamps();
