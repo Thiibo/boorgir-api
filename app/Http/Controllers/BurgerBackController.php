7120a523d965b3b23eaf0f01a0be3faf9598ade8
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-18 19:22:58 +0200

    Add missing image service to BurgerBackController

--- app/Http/Controllers/BurgerBackController.php
+++ app/Http/Controllers/BurgerBackController.php
@@ -3,12 +3,14 @@
 namespace App\Http\Controllers;
 
 use App\Modules\Burgers\Services\BurgerBackService;
+use App\Modules\Burgers\Services\BurgerImageService;
 
 class BurgerBackController extends FullApiServiceController
 {
-    public function __construct(BurgerBackService $service)
+    public function __construct(BurgerBackService $service, BurgerImageService $imageService)
     {
         parent::__construct();
         $this->service = $service;
+        $this->imageService = $imageService;
     }
 }
