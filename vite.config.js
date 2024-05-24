ae797499745d293bbf54d5123d86cfe489070719
Thibo De Vogelaere <thibo.de.vogelaere@student.howest.be>
2024-05-24 19:12:48 +0200

    Create proxy for running Laravel server on Linux

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,9 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
+import mkcert from 'vite-plugin-mkcert';
+
+const host = 'boorgir.test';
+const port = '8000';
 
 export default defineConfig({
     plugins: [
@@ -7,5 +11,21 @@ export default defineConfig({
             input: ['resources/css/app.css', 'resources/js/app.js'],
             refresh: true,
         }),
+        mkcert(),
     ],
+    server: {
+        https: true,
+        proxy: {
+            '^(?!(\/\@vite|\/resources|\/node_modules))': {
+                target: `http://${host}:${port}`
+            }
+        },
+        cors: {
+            origin: 'https://boorgir.test:3000',
+            credentials: true
+        },
+        host,
+        port: 443,
+        hmr: { host },
+    },
 });
